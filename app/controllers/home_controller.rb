49d2d5d1cbc2e0b9b72f1db606fdc90306b1e28a
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-09-05 13:37:59 +0300

    Fixed bug with wrong redirect in home#about

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def about
-    redirect_to surveys_url if user_signed_in?
-    redirect_to admin_root_path if admin_signed_in?
+    if admin_signed_in?
+      redirect_to admin_root_url
+    elsif user_signed_in?
+      redirect_to surveys_url
+    end
   end
 end
--- spec/features/admin/authenticate_spec.rb
+++ spec/features/admin/authenticate_spec.rb
@@ -1,65 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe 'Authentication for admin', type: :feature do
-  describe 'login' do
-    let!(:user) { FactoryGirl.create :user }
-    let!(:admin) { FactoryGirl.create :admin }
+  let!(:user) { FactoryGirl.create :user }
+  let!(:admin) { FactoryGirl.create :admin }
 
-    describe 'successfully' do
-      before do
-	visit new_admin_session_path
-	fill_in "Логин", with: admin.login
-	fill_in "Пароль", with: admin.password
-	click_button "Войти"
+  describe 'successfully' do
+    before do
+      visit new_admin_session_path
+      fill_in "Логин", with: admin.login
+      fill_in "Пароль", with: admin.password
+      click_button "Войти"
+    end
+
+    it 'login' do
+      within '#messages .alert-success' do
+	expect(page).to have_content "Вход в систему осуществлен"
       end
+      expect(page.current_path).to eq admin_root_path
+    end
 
-      it 'login' do
-	within '#messages .alert-success' do
-	  expect(page).to have_content "Вход в систему осуществлен"
-	end
-	expect(page.current_path).to eq admin_root_path
+    it 'logout' do
+      click_link "Выход"
+      within '#messages .alert-success' do
+	expect(page).to have_content "Выход из системы осуществлен"
       end
+      expect(page.current_path).to eq root_path
+    end
+  end
 
-      it 'logout' do
-	click_link "Выход"
-	within '#messages .alert-success' do
-	  expect(page).to have_content "Выход из системы осуществлен"
-	end
-	expect(page.current_path).to eq root_path
+  describe 'unsuccessfully' do
+    before do
+      visit new_admin_session_path
+    end
+
+    after do
+      click_button "Войти"
+      within '#messages .alert-danger' do
+	expect(page).to have_content "Неверный логин или пароль"
       end
     end
 
-    describe 'unsuccessfully' do
-      before do
-	visit new_admin_session_path
-      end
+    it 'login empty' do
+      fill_in "Логин", with: ''
+      fill_in "Пароль", with: admin.password
+    end
 
-      after do
-	click_button "Войти"
-	within '#messages .alert-danger' do
-	  expect(page).to have_content "Неверный логин или пароль"
-	end
-      end
+    it 'login wrong' do
+      fill_in "Логин", with: user.login
+      fill_in "Пароль", with: admin.password
+    end
+    
+    it 'password empty' do
+      fill_in "Логин", with: admin.login
+      fill_in "Пароль", with: ''
+    end
 
-      it 'login empty' do
-	fill_in "Логин", with: ''
-	fill_in "Пароль", with: admin.password
-      end
+    it 'password wrong' do
+      fill_in "Логин", with: admin.login
+      fill_in "Пароль", with: 'wrongpassword'
+    end
+  end
 
-      it 'login wrong' do
-	fill_in "Логин", with: user.login
-	fill_in "Пароль", with: admin.password
-      end
-      
-      it 'password empty' do
-	fill_in "Логин", with: admin.login
-	fill_in "Пароль", with: ''
-      end
-
-      it 'password wrong' do
-	fill_in "Логин", with: admin.login
-	fill_in "Пароль", with: 'wrongpassword'
-      end
+  describe 'signed in behaviour' do
+    it 'redirect to admiin when logged in as admin and user' do
+      visit new_admin_session_path
+      fill_in "Логин", with: admin.login
+      fill_in "Пароль", with: admin.password
+      click_button "Войти"
+      visit new_user_session_path
+      fill_in "Логин", with: user.login
+      fill_in "Пароль", with: user.password
+      click_button "Войти"
+      visit root_path
+      expect(page.current_path).to eq admin_root_path
     end
   end
 end
