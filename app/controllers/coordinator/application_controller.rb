4860a22af54da88475b7cea9f5d9cfbef3c79c0d
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-10-01 12:09:40 +0300

    Completed ugly xls rendering

--- app/controllers/coordinator/application_controller.rb
+++ app/controllers/coordinator/application_controller.rb
@@ -8,26 +8,4 @@ class Coordinator::ApplicationController < ApplicationController
   def surveys_export
     render xls: Survey.export_xls, name: "Опросы #{Time.now.strftime '%d.%m.%Y'}"
   end
-
-  private
-  
-  def count_first_questions
-    max = Response.on_first_questions.max do |a,b|
-      a.question_number <=> b.question_number
-    end.question_number
-    min = Response.on_first_questions.min do |a,b|
-      a.question_number <=> b.question_number
-    end.question_number
-    max - min + 1
-  end
-
-  def count_second_questions
-    max = Response.on_second_questions.max do |a,b|
-      a.question_number <=> b.question_number
-    end.question_number
-    min = Response.on_second_questions.min do |a,b|
-      a.question_number <=> b.question_number
-    end.question_number
-    max - min + 1
-  end
 end
