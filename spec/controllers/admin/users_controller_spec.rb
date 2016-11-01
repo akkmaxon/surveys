0577d72d41b709ebcea95b3cfe0a800dd73ed053
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-11-01 13:10:35 +0300

    Specs for admin/users controller

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,22 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :controller do
+  let(:admin) { FactoryGirl.create :admin }
+  let!(:user) { FactoryGirl.create :user }
+
+  before do
+    sign_in admin
+  end
+
   describe 'GET #index' do
-    it 'successfully'
-    it 'redirect to sign in'
+    it 'successfully' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'redirect to sign in' do
+      sign_out :admin
+      get :index
+      expect(response).to redirect_to(:new_admin_session)
+      expect(flash[:alert]).to eq("Войдите, пожалуйста, в систему.")
+    end
   end
+
   describe 'GET #show' do
-    it 'successfully'
-    it 'redirect to sign in'
+    it 'redirect to sign in' do
+      sign_out :admin
+      get :show, params: { id: user.login }
+      expect(response).to redirect_to(:new_admin_session)
+      expect(flash[:alert]).to eq("Войдите, пожалуйста, в систему.")
+    end
   end
+
   describe 'POST #create' do
-    it 'successfully'
-    it 'fails when empty params'
-    it 'redirect to sign in'
+    it 'successfully' do
+      post :create, params: { user: { login: "Login", password: "password" } }
+      expect(response).to redirect_to(:admin_users)
+      expect(flash[:notice]).to eq("Респондент создан.")
+    end
+
+    it 'fails when empty params' do
+      post :create, params: { user: { login: "", password: "" } }
+      expect(response).to render_template(:index)
+      expect(flash).to be_empty
+    end
+
+    it 'redirect to sign in' do
+      sign_out :admin
+      post :create, params: { user: { login: "Login", password: "password" } }
+      expect(response).to redirect_to(:new_admin_session)
+      expect(flash[:alert]).to eq("Войдите, пожалуйста, в систему.")
+    end
   end
+
   describe 'PUT #update' do
-    it 'successfully'
-    it 'fails when empty params'
-    it 'redirect to sign in'
+    it 'successfully' do
+      put :update, params: { id: user.login, user: { login: "New Login", password: "newpassword" } }
+      expect(response).to redirect_to(:admin_users)
+      expect(flash[:notice]).to eq("Респондент изменен.")
+    end
+
+    it 'fails when empty params' do
+      put :update, params: { id: user.login, user: { login: "", password: "" } }
+      expect(response).to render_template(:index)
+      expect(flash).to be_empty
+    end
+
+    it 'redirect to sign in' do
+      sign_out :admin
+      put :update, params: { id: user.login, user: { login: "New Login", password: "newpassword" } }
+      expect(response).to redirect_to(:new_admin_session)
+      expect(flash[:alert]).to eq("Войдите, пожалуйста, в систему.")
+    end
   end
 end
