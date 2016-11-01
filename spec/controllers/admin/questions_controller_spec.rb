8dda04d69aad0b73b22b6bf288eaa6685942cc66
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-11-01 14:22:08 +0300

    Specs on admin/questions controller

--- spec/controllers/admin/questions_controller_spec.rb
+++ spec/controllers/admin/questions_controller_spec.rb
@@ -1,22 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Admin::QuestionsController, type: :controller do
+  let(:admin) { FactoryGirl.create :admin }
+  let!(:question) { FactoryGirl.create :question }
+  let!(:lst) { FactoryGirl.create :left_statement, question: question }
+  let!(:rst) { FactoryGirl.create :right_statement, question: question }
+  let(:question_params) do
+    { question: FactoryGirl.attributes_for(:question),
+      left_title: FactoryGirl.attributes_for(:left_statement)[:title],
+      left_text: FactoryGirl.attributes_for(:left_statement)[:text],
+      right_title: FactoryGirl.attributes_for(:right_statement)[:title],
+      right_text: FactoryGirl.attributes_for(:right_statement)[:text] }
+  end
+
+  before do
+    sign_in admin
+  end
+
   describe 'GET #index' do
-    it 'successfully'
-    it 'redirect to sign in'
+    it 'successfully' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'redirect to sign in' do
+      sign_out admin
+      get :index
+      expect(response).to redirect_to(:new_admin_session)
+      expect(flash[:alert]).to eq("Войдите, пожалуйста, в систему.")
+    end
   end
+
   describe 'POST #create' do
-    it 'successfully'
-    it 'fails when empty params'
-    it 'redirect to sign in'
+    it 'successfully' do
+      post :create, params: question_params
+      expect(response).to redirect_to(:admin_questions)
+      expect(flash[:notice]).to eq("Вопрос создан.")
+    end
+
+    it 'redirect to sign in' do
+      sign_out admin
+      post :create, params: question_params
+      expect(response).to redirect_to(:new_admin_session)
+      expect(flash[:alert]).to eq("Войдите, пожалуйста, в систему.")
+    end
   end
+
   describe 'PUT #update' do
-    it 'successfully'
-    it 'fails when empty params'
-    it 'redirect to sign in'
+    it 'successfully' do
+      put :update, params: question_params.merge({ id: question.id })
+      expect(response).to redirect_to(:admin_questions)
+      expect(flash[:notice]).to eq("Вопрос обновлен.")
+    end
+
+    it 'redirect to sign in' do
+      sign_out admin
+      put :update, params: question_params.merge({ id: question.id })
+      expect(response).to redirect_to(:new_admin_session)
+      expect(flash[:alert]).to eq("Войдите, пожалуйста, в систему.")
+    end
   end
+
   describe 'DELETE #destroy' do
-    it 'successfully'
-    it 'redirect to sign in'
+    it 'successfully' do
+      delete :destroy, params: { id: question.id }
+      expect(response).to redirect_to(:admin_questions)
+      expect(flash[:notice]).to eq("Вопрос удален.")
+    end
+
+    it 'redirect to sign in' do
+      sign_out admin
+      delete :destroy, params: { id: question.id }
+      expect(response).to redirect_to(:new_admin_session)
+      expect(flash[:alert]).to eq("Войдите, пожалуйста, в систему.")
+    end
   end
 end
