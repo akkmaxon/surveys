9a453062518d66e01fddc28cea2edfb9419423f4
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-11-01 14:56:27 +0300

    Specs on coordinators/users controller

---
+++ spec/controllers/coordinator/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Coordinator::UsersController, type: :controller do
+  describe 'GET #index' do
+    let!(:coordinator) { FactoryGirl.create :coordinator }
+
+    it 'successfully' do
+      sign_in coordinator
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'redirect when not logged in' do
+      get :index
+      expect(response).to redirect_to(:new_coordinator_session)
+      expect(flash[:alert]).to eq("Войдите, пожалуйста, в систему.")
+    end
+  end
+end
