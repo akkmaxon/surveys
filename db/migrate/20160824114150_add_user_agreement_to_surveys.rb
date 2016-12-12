7a1cc6a5f421676ac68e8d608c920fa9bb5d99a0
Akkuzin Maxim <akkmaxon2307@gmail.com>
2016-12-12 14:48:40 +0300

    Changed defaults for email and agreement by survey

--- db/migrate/20160824114150_add_user_agreement_to_surveys.rb
+++ db/migrate/20160824114150_add_user_agreement_to_surveys.rb
@@ -1,5 +1,5 @@
 class AddUserAgreementToSurveys < ActiveRecord::Migration[5.0]
   def change
-    add_column :surveys, :user_agreement, :string, default: ""
+    add_column :surveys, :user_agreement, :string, default: "-"
   end
 end
